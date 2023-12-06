e32c24354d376a314744c8fcea956cd6b67a2083
norikois <33947244+norikois@users.noreply.github.com>
2023-12-06 17:25:13 +0900

    初回コミット
    初回コミット

---
+++ KB12429_WpfApp1/Model/Person.cs
@@ -0,0 +1,30 @@
+﻿using KB12429_WpfApp1.Infrastructure;
+
+namespace KB12429_WpfApp1.Model;
+internal class Person : ObservableObject
+{
+    private String _name = String.Empty;
+    public String Name
+    {
+        get { return _name; }
+        set { _name = value; OnPropertyChanged(); }
+    }
+
+    private String _prefectureCode = String.Empty;
+    public String PrefectureCode
+    {
+        get { return _prefectureCode; }
+        set { _prefectureCode = value; OnPropertyChanged(); }
+    }
+
+    private String _description = String.Empty;
+    public String Description
+    {
+        get { return _description; }
+        set { _description = value; OnPropertyChanged(); }
+    }
+
+    public Person()
+    {
+    }
+}
---
+++ KB12429_WpfApp1/Model/Prefecture.cs
@@ -0,0 +1,23 @@
+﻿using KB12429_WpfApp1.Infrastructure;
+
+namespace KB12429_WpfApp1.Model;
+internal class Prefecture : ObservableObject
+{
+    private String _prefectureCode = String.Empty;
+    public String PrefectureCode
+    {
+        get { return _prefectureCode; }
+        set { _prefectureCode = value; OnPropertyChanged(); }
+    }
+
+    private String _prefectureName = String.Empty;
+    public String PrefectureName
+    {
+        get { return _prefectureName; }
+        set { _prefectureName = value; OnPropertyChanged(); }
+    }
+
+    public Prefecture()
+    {
+    }
+}
